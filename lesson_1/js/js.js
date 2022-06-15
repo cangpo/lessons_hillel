20b934b7316fb30eb9a42907ec6244e43b2df71e
Alex Dihtiar <cangpo@gmail.com>
2022-06-15 12:37:27 +0300

    Lesson 1

---
+++ lesson_1/js/js.js
@@ -0,0 +1,42 @@
+let userName = prompt('Hello, enter your name');
+let userSurname = prompt('Enter your surname');
+let userAge = prompt('Enter your age');
+let userHobby = prompt('Enter your hobby');
+
+let userData = {
+  name: userName,
+  surname: userSurname,
+  age: userAge,
+  hobby: userHobby,
+}
+
+alert(userData.name + ' ' + userData.surname + ' ' + userData.age + ' ' + userData.hobby);
+console.log(userData.name + ' ' + userData.surname + ' ' + userData.age + ' ' + userData.hobby);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
